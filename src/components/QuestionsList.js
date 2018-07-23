81a9e0f565bdac940079105eef335a2c6a4c5deb
blitnee <brittneelc@gmail.com>
2018-07-23 14:52:38 -0400

    feat: Add Tabs for Questions Lists

--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react'
 import QuestionPrev from './QuestionPrev'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class QuestionsList extends Component {
+
+	// @todo: Return the avatarURL for the user corresponding to the question author
+	getAvatar (author) {
+		this.props.users.filter((u) => {
+			if(u.id === author) {
+				return u.avatarURL
+			}
+		})
+	}
+
 	render() {
-		console.log(this.props.questions)
+		{/* @todo: Add button prop for vote or pole functionality */}
 		return (
 			<div className='questions-list container-qlist'>
-				<div id="unanswered-questions-tab" className="tabcontent">
-				  <h2>Unanswered Questions</h2>
-				{/* @todo: List based on user unanswered */}
-				  	{this.props.questions.map((id) => {
-				  		return <QuestionPrev key={id} />
-				  	})}
-				</div>
-				<div id="answered-questions-tab" className="tabcontent">
-				  <h2>Answered Questions</h2>
-				{/* @todo: List based on user answered */}
-				  	{this.props.questions.map((id) => {
-				  		return <QuestionPrev key={id} />
-				  	})}
-				</div>
+			<Tabs>
+		    <TabList className='tab-list container-element'>
+		      <Tab className='tab container-qlist'>Unanswered Questions</Tab>
+		      <Tab className='tab container-qlist'>Answered Questions</Tab>
+		    </TabList>
+
+				{/* @todo: Lists based on user unanswered */}
+		    <TabPanel>
+	      	{this.props.questions.map((q) => {
+			  		return <QuestionPrev key={q.id} author={q.author} optionOne={q.optionOne.text} optionTwo={q.optionTwo.text} />
+			  	})}
+		    </TabPanel>
+		    <TabPanel>
+	      	{this.props.questions.map((q) => {
+			  		return 'this works...'
+			  	})}
+		    </TabPanel>
+			</Tabs>
 			</div>
 		)
 	}
