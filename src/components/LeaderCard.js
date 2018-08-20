50911c6838431bd4e73235e0af2e8e52f957b395
blitnee <brittneelc@gmail.com>
2018-08-20 17:24:27 -0400

    feat: Add LeaderCard Component

---
+++ src/components/LeaderCard.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class LeaderCard extends Component {
+	render() {
+		const { name, index, answers, questions, score } = this.props
+		return (
+			<div className='leader-card container-element'>
+				<div className='leader-rank'>{index}</div>
+				<div className='leader-info'>
+					<div className='leader-header'>
+						<img src="avatar" />
+						<h3 className='leader-card-title'>{name}</h3>
+					</div>
+					<div className='leader-stats-container'>
+						<div className='leader-score'>Score: {score}</div>
+						<div className='leader-answer-stats'>Answers: {answers}</div>
+						<div className='leader-question-stats'>Questions: {questions}</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default LeaderCard
