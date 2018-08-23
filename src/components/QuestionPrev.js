25d0298f04f908f477c8d680eefac979e55f0692
blitnee <brittneelc@gmail.com>
2018-08-23 12:48:50 -0400

    feat: Add submit/results button with redirect

--- src/components/QuestionPrev.js
+++ src/components/QuestionPrev.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import { saveQuestionAnswer } from '../actions/questions'
+import { Link } from 'react-router-dom'
 
 class QuestionPrev extends Component {
 
+	handleClick = (vote) => {
+		//dispatch(saveQuestionAnswer(this.state.vote))
+	}
+
+	getButtonType = (value) => {
+		return value === 'Submit'
+			? <button className='pole-button container-element'>{value}</button>
+			: <Link to={`/question/${this.props.id}`}><button className='pole-button container-element'>{value}</button></Link>
+	}
+
 	render() {
+		const { id, avatarURL, author, optionOne, optionTwo, buttonValue  }  = this.props
 		return (
 			<div className='question-card container-element'>
 				<h3 className='question-card-title container-element'>
-					<div style={{ backgroundImage: `url(${this.props.avatarURL})` }} />
-					<span>{this.props.author}</span>
+				 {/* <div style={{ backgroundImage: `url(${avatarURL})` }} /> */}
+					<img src={avatarURL} />
+					<span>{author}</span>
 				</h3>
 				<div className="block-container">
 				{/* @todo: Onclick send to '/question/#/vote' */}
@@ -19,26 +33,27 @@ class QuestionPrev extends Component {
 								<input
 									id='option-one'
 									type='radio'
-									// value={this.props.optionOne}
-									onChange={this.handleClick(this.props.optionOne.text)}
+									value={this.props.optionOne}
+									// onChange={this.handleClick(optionOne.text)}
 								/>
-								{this.props.optionOne}
+								{optionOne}
 							</label>
 							<label htmlFor='option-two' className='question-option-label'>
 								<input
 									id='option-two'
 									type='radio'
-									// value={this.props.optionTwo}
-									onChange={this.handleClick(this.props.optionTwo.text)}
+									value={this.props.optionTwo}
+									// onChange={this.handleClick(optionTwo.text)}
 								/>
-								{this.props.optionTwo}
+								{optionTwo}
 							</label>
 						</div>
 					</div>
 				</div>
 				<div className="button-container">
 				{/* @todo: Onclick send to '/question/#/results' */}
-					<button className='pole-button container-element'>{this.props.buttonValue}</button>
+					{this.getButtonType(buttonValue)}
+
 				</div>
 			</div>
 		)
--- src/components/QuestionsList.js
+++ src/components/QuestionsList.js
@@ -4,48 +4,50 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class QuestionsList extends Component {
 
-	// @todo: Return the avatarURL for the user corresponding to the question author
 	getAvatar (author) {
-		this.props.users.filter((u) => {
-			if(u.id === author) {
-				return u.avatarURL
-			}
+		let user = this.props.users.filter((u) => {
+			return u.id === author.author
 		})
+		return user[0].avatarURL // @todo: Fix this.
 	}
 
 	render() {
-		{/* @todo: Add button prop for vote or pole functionality */}
+		const { unanswered, answered, users } = this.props
 		return (
 			<div className='questions-list container-qlist'>
 				<Tabs>
 			    <TabList className='tab-list container-element'>
-			      <Tab className='tab container-qlist'>Unanswered Questions</Tab>
-			      <Tab className='tab container-qlist'>Answered Questions</Tab>
+			      <Tab className='tab container-qlist'>
+			      		 Unanswered Questions</Tab>
+			      <Tab className='tab container-qlist'>
+			      		 Answered Questions</Tab>
 			    </TabList>
-					{/* @todo: Lists based on user unanswered */}
 			    <TabPanel>
-		      	{ this.props.unanswered.map((q) => {
+		      	{ unanswered.map((q) => {
 				  		return <QuestionPrev
 				  							key={q.id}
+				  							id={q.id}
 				  							author={q.author}
-				  							avatarURL={this.getAvatar(q.author)}
+				  							avatarURL={this.getAvatar(q)}
 				  							optionOne={q.optionOne.text}
 				  							optionTwo={q.optionTwo.text}
 				  							onHandleSubmit={this.props.onHandleSubmit}
 				  							onChange={this.props.onUserVote}
-				  							buttonValue={'Submit'} />
-						})}
+				  							buttonValue={'Submit'}
+				  							/>})}
 			    </TabPanel>
 			    <TabPanel>
-		      	{this.props.answered.map((q) => {
+		      	{answered.map((q) => {
 				  		return <QuestionPrev
 				  							key={q.id}
+				  							id={q.id}
+				  							users={users}
 				  							author={q.author}
-				  							avatar={this.getAvatar(q.author)}
+				  							avatarURL={this.getAvatar(q)}
 				  							optionOne={q.optionOne.text}
 				  							optionTwo={q.optionTwo.text}
-				  							buttonValue={'Pole Results'} />
-				  	})}
+				  							buttonValue={'Results'}
+				  					/>})}
 			    </TabPanel>
 				</Tabs>
 			</div>
