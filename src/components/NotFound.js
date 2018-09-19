f2152d572645e252c04ee54bfcc8e0e5779615b4
blitnee <brittneelc@gmail.com>
2018-09-19 13:19:39 -0400

    refactor: Pure component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,25 +1,14 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
-class NotFound extends Component {
-
-	render() {
-		return (
-			<div className='not-found-container container-element'>
-				<h1 className='not-found-error'>404</h1>
-				<h2 className='not-found-title'>Page Not Found</h2>
-				<Link to="/"><p className='not-found-link'>&larr; Back to the App</p></Link>
-			</div>
-		)
-	}
+const NotFound = () => {
+	return (
+		<div className='not-found-container container-element'>
+			<h1 className='not-found-error'>404</h1>
+			<h2 className='not-found-title'>Page Not Found</h2>
+			<Link to="/"><p className='not-found-link'>&larr; Back to the App</p></Link>
+		</div>
+	)
 }
 
-function mapStateToProps ({ questions, users }) {
-	return {
-		questions: Object.values(questions),
-		users: Object.values(users)
-	}
-}
-
-export default connect(mapStateToProps)(NotFound)
+export default NotFound
